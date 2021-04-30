63cfbb51ed6a83203869b9287a1d8ae7174d65af
dbdudrnr97 <dbdudrnr97@naver.com>
2021-04-30 15:45:38 +0900

    search menu type

---
+++ src/main/java/com/kh/spring/menu/model/dao/MenuDaoImpl.java
@@ -0,0 +1,26 @@
+package com.kh.spring.menu.model.dao;
+
+import java.util.List;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.kh.spring.menu.model.vo.Menu;
+
+@Repository
+public class MenuDaoImpl implements MenuDao {
+
+	@Autowired
+	private SqlSessionTemplate session;
+
+	@Override
+	public List<Menu> selectMenuList() {
+		return session.selectList("menu.selectMenuList");
+	}
+
+	@Override
+	public List<Menu> selectMenuType(String type) {
+		return session.selectList("menu.selectMenuType",type);
+	}
+}
---
+++ src/main/java/com/kh/spring/menu/model/service/MenuService.java
@@ -0,0 +1,12 @@
+package com.kh.spring.menu.model.service;
+
+import java.util.List;
+
+import com.kh.spring.menu.model.vo.Menu;
+
+public interface MenuService {
+	List<Menu> selectMenuList();
+
+	List<Menu> selectMenuType(String type);
+
+}
